1f45efb7e759522730c44b304b3017fff0adf614
jadu lance <simponski@gmail.com>
2025-02-21 15:26:54 +0000

    added saving files, a couple of bugs

--- notepad--/src/main.py
+++ notepad--/src/main.py
@@ -1,24 +1,45 @@
 import flet as ft;
 import os;
 
+
 class NoteView():
     def __init__(self, page):
         self.page = page;
         self.textarea = ft.Ref[ft.TextField]();
         self.menubar = ft.Ref[ft.MenuBar]();
+
         self.filepicker = ft.FilePicker(on_result = self.pick_files_result);
         self.page.overlay.append(self.filepicker);
+        self.current_action = None;
+
+        self.current_file = None;
+        self.file_name = "";
         self.create();
 
     def pick_files_result(self, e: ft.FilePickerResultEvent):
-        if e.path != None:
-            new_file = os.open(e.path, os.O_WRONLY | os.O_CREAT);
-            os.write(new_file, self.textarea.current.value.encode());
-        else:
-            print(e.files)
+        if self.current_action == "create_file":
+            self.current_file = f"{e.path}/{self.file_name}";
+            self.create_file(self.current_file, self.textarea.current.value);
+            
+    def create_file(self, file, content):
+        open(file, "w").write(content);
+        self.current_action = "none";
 
-    def new_file(self, e):
-        self.filepicker.save_file();
+    def save_clicked(self, e):
+        if self.current_file is None:
+            self.current_action = "create_file";
+            self.ask_file_name();
+        else:
+            self.create_file(self.current_file, self.textarea.current.value);
+    
+    def file_name_submitted(self, e):
+        self.file_name = e.data;
+        self.filepicker.get_directory_path();
+
+    def ask_file_name(self):
+        self.page.open(ft.BottomSheet(
+            content = ft.TextField(hint_text = "file name", on_submit = self.file_name_submitted),
+        ));
 
     def create_text_area(self):
         return ft.TextField(ref = self.textarea, border = ft.InputBorder.NONE, multiline = True, hint_text = "insert notes here");
@@ -30,18 +51,14 @@ class NoteView():
             controls = [
                 ft.SubmenuButton(
                     content = ft.Text("File"),
-                    controls = [
-                        ft.MenuItemButton(
-                            content = ft.Text("New"),
-                            on_click = self.new_file,
+                    controls = [      
+                         ft.MenuItemButton(
+                            content = ft.Text("Save"),
+                            on_click = self.save_clicked,
                         ),
 
                          ft.MenuItemButton(
                             content = ft.Text("Open")
-                        ),
-
-                         ft.MenuItemButton(
-                            content = ft.Text("Save")
                         ),
                     ]
                 ),
